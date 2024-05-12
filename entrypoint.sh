621e0bec493908f1b3107601b029017541820d15
Antonín Kazda <antoninkazda@seznam.cz>
2024-05-12 15:46:21 +0200

    feat: update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,7 @@ echo "Running DeeplTranslateTool with arguments $*"
 printf '%d args:' "$#"
 printf " '%s'" "$@"
 printf '\n'
+
+git clone https://github.com/kazda01/deepl-translate-tool.git -o DeeplTranslateTool
+
+dotnet run --project DeeplTranslateTool/DeeplTranslateTool
