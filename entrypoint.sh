b14276e766e7753749e965c33a6a7cd2c3cbfb8f
Antonín Kazda <antoninkazda@seznam.cz>
2024-05-12 15:57:23 +0200

    feat: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,24 @@ printf '%d args:' "$#"
 printf " '%s'" "$@"
 printf '\n'
 
-git clone https://github.com/kazda01/deepl-translate-tool.git -o DeeplTranslateTool
+# Extract input parameters
+API_KEY=$1
+ADAPTER=$2
+SOURCE_LANGUAGE=$3
+LANGUAGES=$4
+INPUT_FILE=$5
+PATH=$6
+NO_CACHE=$7
 
-dotnet run --project DeeplTranslateTool/DeeplTranslateTool
+# Use the input parameters as needed
+echo "API Key: $API_KEY"
+echo "Adapter: $ADAPTER"
+echo "Source Language: $SOURCE_LANGUAGE"
+echo "Languages: $LANGUAGES"
+echo "Input File: $INPUT_FILE"
+echo "Path: $PATH"
+echo "No Cache: $NO_CACHE"
+
+git clone https://github.com/kazda01/deepl-translate-tool.git
+
+dotnet run --project deepl-translate-tool/DeeplTranslateTool
